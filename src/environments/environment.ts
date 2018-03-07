2c5270d6b8b7ed903279bac6717256c4fa3622c4
Alex1664 <alex.vacheret@free.fr>
2018-03-07 09:30:04 +0100

    first commit Alex

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAMSJCoYGrD_cNWzM5L2P2F08BpPZXSZzM',
+    authDomain: 'crypto-simulator-404.firebaseapp.com',
+    databaseURL: 'https://crypto-simulator-404.firebaseio.com',
+    projectId: 'crypto-simulator-404',
+    storageBucket: 'crypto-simulator-404.appspot.com',
+    messagingSenderId: '845858549210'
+  }
 };
