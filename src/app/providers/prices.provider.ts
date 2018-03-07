37fd5acd2cc6d8cd94b32821812f723967e38afb
erwanlbp <erwan.lbp@gmail.com>
2018-03-07 14:45:32 +0100

    getLastPrice(symbol

--- src/app/providers/prices.provider.ts
+++ src/app/providers/prices.provider.ts
@@ -2,6 +2,8 @@ import {Injectable} from '@angular/core';
 import {IntervalObservable} from 'rxjs/observable/IntervalObservable';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/take';
+import * as _ from 'underscore';
 
 @Injectable()
 export class PRICESProvider {
@@ -9,9 +11,15 @@ export class PRICESProvider {
   constructor(private client: HttpClient) {
   }
 
-  public getPrices(): Observable<any> {
+  getPrices(): Observable<any> {
     return IntervalObservable
       .create(2000)
       .flatMap(() => this.client.get(`https://api.binance.com/api/v3/ticker/price`));
+  }
+
+  getLastPrice(symbol) {
+    return this.getPrices()
+      .map(coins => _.find(coins, e => e.symbol === symbol))
+      .take(1);
   }
 }
