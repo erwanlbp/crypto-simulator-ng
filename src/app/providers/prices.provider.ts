52c6f83120709ea3a5b179ba9820dec51a54becb
erwanlbp <erwan.lbp@gmail.com>
2018-03-07 14:09:47 +0100

    bug fixing

--- src/app/providers/prices.provider.ts
+++ src/app/providers/prices.provider.ts
@@ -1,32 +1,17 @@
 import {Injectable} from '@angular/core';
 import {IntervalObservable} from 'rxjs/observable/IntervalObservable';
 import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs/Observable';
 
 @Injectable()
 export class PRICESProvider {
 
-  public static prices = {};
-
   constructor(private client: HttpClient) {
-    IntervalObservable
+  }
+
+  public getPrices(): Observable<any> {
+    return IntervalObservable
       .create(2000)
-      .subscribe(
-        () => {
-          this.client.get(`https://api.binance.com/api/v3/ticker/price?`)
-            .subscribe(
-              (coins: any[]) => coins.forEach((c: any) => {
-                this.updatePrices(c.symbol, c.price);
-              })
-            );
-        });
+      .flatMap(() => this.client.get(`https://api.binance.com/api/v3/ticker/price`));
   }
-
-  public updatePrices(coinSymbol, value) {
-    if (!PRICESProvider.prices[coinSymbol]) {
-      PRICESProvider.prices[coinSymbol] = [];
-    }
-
-    PRICESProvider.prices[coinSymbol] = [...PRICESProvider.prices[coinSymbol], value];
-  }
-
 }
