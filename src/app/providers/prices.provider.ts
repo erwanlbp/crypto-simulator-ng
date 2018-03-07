9ca445b0061d2e73dcf0796e2ea6513f5c245fbe
erwanlbp <erwan.lbp@gmail.com>
2018-03-07 13:34:43 +0100

    Fix get-price

--- src/app/providers/prices.provider.ts
+++ src/app/providers/prices.provider.ts
@@ -1,9 +1,25 @@
 import {Injectable} from '@angular/core';
+import {IntervalObservable} from 'rxjs/observable/IntervalObservable';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable()
 export class PRICESProvider {
 
   public static prices = {};
+
+  constructor(private client: HttpClient) {
+    IntervalObservable
+      .create(2000)
+      .subscribe(
+        () => {
+          this.client.get(`https://api.binance.com/api/v3/ticker/price?`)
+            .subscribe(
+              (coins: any[]) => coins.forEach((c: any) => {
+                this.updatePrices(c.symbol, c.price);
+              })
+            );
+        });
+  }
 
   public updatePrices(coinSymbol, value) {
     if (!PRICESProvider.prices[coinSymbol]) {
