b11938eacb6f6b805c1dbfe1118df2a762fff46a
erwanlbp <erwan.lbp@gmail.com>
2018-03-07 15:42:57 +0100

    Fire order

--- src/app/components/fire-order/fire-order.component.ts
+++ src/app/components/fire-order/fire-order.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {AngularFirestore} from 'angularfire2/firestore';
-import {FireBalanceProvider} from '../../providers/fire-balance/fire-balance.provider';
+import {FireBalanceProvider} from '../../providers/fire-balance.provider';
+import {PRICESProvider} from '../../providers/prices.provider';
 
 @Component({
   selector: 'app-fire-order',
@@ -8,11 +9,17 @@ import {FireBalanceProvider} from '../../providers/fire-balance/fire-balance.pro
   styleUrls: ['./fire-order.component.css']
 })
 export class FireOrderComponent implements OnInit {
-  quantity: number = 0;
+  coin: string;
+  coinFrom: string;
+  quantityCoin: number = 0;
+  quantityCoinFrom: number = 0;
   symbol: string;
+  symbolPrice: number = -1;
   email: string = 'erwan.lbp@gmail.com';
 
-  constructor(private db: AngularFirestore, private balances$: FireBalanceProvider) {
+  constructor(private db: AngularFirestore,
+              private balances$: FireBalanceProvider,
+              private pricesProvider: PRICESProvider) {
   }
 
   ngOnInit() {
@@ -22,17 +29,52 @@ export class FireOrderComponent implements OnInit {
     if (this.symbol.length === 0) {
       return;
     }
+
     this.balances$.getBalances(this.email)
       .subscribe((e: Balance[]) => {
         const balanceArr = e.filter(bal => this.symbol.endsWith(bal.coin));
         if (balanceArr.length === 0) {
           return;
         }
-        this.quantity = percent * balanceArr[0].balance;
+        this.quantityCoinFrom = percent * balanceArr[0].balance;
+        this.quantityCoin = this.quantityCoinFrom / this.symbolPrice;
       });
   }
 
   placeOrder() {
 
+  }
+
+  priceOfSymbol() {
+    this.pricesProvider.getLastPrice(this.symbol)
+      .subscribe((e: any) => {
+        console.log(e);
+        if (e['price']) {
+          this.symbolPrice = e.price;
+        }
+      });
+  }
+
+  splitSymbol() {
+    if (this.symbol.length === 0) {
+      return;
+    }
+
+    this.balances$.getBalances(this.email)
+      .subscribe((e: Balance[]) => {
+        const balanceArr = e.filter(bal => this.symbol.endsWith(bal.coin));
+        if (balanceArr.length !== 0) {
+          this.coinFrom = balanceArr[0].coin;
+          this.coin = this.symbol.split(this.coinFrom)[0];
+        } else {
+          this.coinFrom = '';
+          this.coin = '';
+        }
+      });
+  }
+
+  symbolChanged() {
+    this.splitSymbol();
+    this.priceOfSymbol();
   }
 }
